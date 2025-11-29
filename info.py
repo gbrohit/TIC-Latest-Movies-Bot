cfadc9f2b7aa582fb57561356528e426f3cd7f32
gbrohit <119616499+gbrohit@users.noreply.github.com>
2025-11-29 14:20:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,8 +49,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', '')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'eadf9fdd64d92e0e49c129aa37d16b70513c45ae')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkshortify.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '3d2381ae2529f67464a13e6b4390e4cca9d0b478')
 SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'gplinks.com')
 SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '2c8b158c80fd0cb0da24974806c823375a40ec5a')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
